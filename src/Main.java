e0a240803829070513f91d9b78426d7faa2ccc9c
udropper <gui.constantino@gmail.com>
2023-07-07 09:07:57 -0300

    main class added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        PrintSpooler printSpooler = new PrintSpooler("spool.txt");
+
+        // Abre o arquivo de spool
+        printSpooler.openPrintSpooler();
+
+        // Caminho para a pasta que contém os arquivos de job
+        String jobFolderPath = "C:/path/to/jobs";
+
+        // Localiza todos os arquivos de job na pasta
+        File jobFolder = new File(jobFolderPath);
+        File[] jobFiles = jobFolder.listFiles();
+
+        // Verifica se há arquivos de job encontrados
+        if (jobFiles != null && jobFiles.length > 0) {
+            // Cria e inicia as threads responsáveis pela submissão das requisições de
+            // impressão
+            int numThreads = jobFiles.length; // Número de threads igual ao número de arquivos de job
+            Thread[] threads = new Thread[numThreads];
+
+            for (int i = 0; i < numThreads; i++) {
+                final String jobPath = jobFiles[i].getAbsolutePath();
+                threads[i] = new Thread(() -> {
+                    try {
+                        Thread.sleep((long) (Math.random() * 5000)); // Espera um período de tempo aleatório
+
+                        printSpooler.printJob(jobPath);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+                threads[i].start();
+            }
+
+            // Aguarda todas as threads concluírem
+            for (int i = 0; i < numThreads; i++) {
+                try {
+                    threads[i].join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            System.out.println("Nenhum arquivo de job encontrado na pasta: " + jobFolderPath);
+        }
+
+        // Fecha o arquivo de spool
+        printSpooler.closePrintSpooler();
     }
 }
